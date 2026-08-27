c1b902c7443d8ab67c457fa5122d67eb8e0fae38
root <aniruddhyak@gmail.com>
2026-08-27 01:41:50 +0000

    Add EF migrations and SearchHost Docker support

--- src/SearchHost/Program.cs
+++ src/SearchHost/Program.cs
@@ -25,6 +25,12 @@ builder.Services.AddSearchModule();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
